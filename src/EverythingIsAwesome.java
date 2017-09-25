93c0c83b9e0d8cab1327c88e7da0607eee864ece
DanielR1 <activatingredstone@gmail.com>
2017-09-24 21:31:34 -0700

    I added close
    -daniel

--- src/EverythingIsAwesome.java
+++ src/EverythingIsAwesome.java
@@ -4,6 +4,7 @@ public class EverythingIsAwesome {
 public static void main(String[] args) {
 	String ans=JOptionPane.showInputDialog("What do you like?");
 	JOptionPane.showMessageDialog(null, ans +" is awesome");
+	System.exit(0);
 	
 	
 	
